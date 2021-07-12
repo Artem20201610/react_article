7b1b326401d76b022d11c557433c78cdd10a038b
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-12 17:21:11 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.e575c0d8489b2c91727bd6a2e94a44a4.js"
 );
 
 self.addEventListener('message', (event) => {
